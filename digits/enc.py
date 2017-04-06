3968d8ed230841da6995bfa4bb66c0bdd7abde0f
Rob Baxter <rbaxter8@gatech.edu>
2017-04-05 21:53:33 -0400

    grid

--- digits/enc.py
+++ digits/enc.py
@@ -65,6 +65,78 @@ def nn():
     score = clf.score(X_test_enc, y_test)
     print(score)
     
+    
+def logistic():
+    
+    train = pd.read_csv("train.csv")
+    y = train.values[:,0]
+    X = train.values[:,1:]
+    
+    # split the training data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.333)
+    
+    #X_train = X_train[:,200:202]
+    #X_test = X_test[:,200:202]
+    
+    #np.ceil(X_train / 10.0) * 10
+    
+    #X_train= np.ceil(X_train / 10.0) * 10
+    #X_test = np.ceil(X_test / 10.0) * 10
+    
+    # there are 784 dimensions (1 for each pixel)
+    # each d has labels 0 to 255
+    # one hot encode for each label
+    labels = np.arange(0,256,1)
+    enc_temp = labels.repeat(X_train.shape[1]).reshape((labels.shape[0], X_train.shape[1]))
+    enc = OneHotEncoder(dtype=np.int8)
+    enc.fit(enc_temp)
+    #enc.n_values_
+    #enc.feature_indices_
+    X_train_enc = enc.transform(X_train).toarray()
+    
+    
+    
+    # question-can i eliminate any rows where the sum is 0? i wonder
+    #X_train_enc.sum(0)
+    
+    Cs = [100, 75, 50, 25, 1]
+    solvers = ['newton-cg', 'lbfgs', 'sag']
+    #solvers = ['liblinear']
+    all_scores = []
+    for solver in solvers:
+        for C in Cs:
+            # have to split up the training because of memory limitations
+            clf = LogisticRegression(verbose=True, C=C, solver=solver, warm_start=True, n_jobs=-1)
+            clf.fit(X_train_enc, y_train)        
+            
+            # free memory
+            #X_train_enc = None
+            
+            X_test_enc = enc.transform(X_test).toarray()
+            print("scoring: ")
+            score = clf.score(X_test_enc, y_test)
+            print(score)
+            
+            all_scores.append((C, solver, score))
+            
+            with open(r'test.txt', 'w') as f:
+                f.write(" ".join(map(str, all_scores)))
+    
+    #print(cross_val_score(clf, X_train_enc, y_train, n_jobs=-1))
+    
+    #kfold = StratifiedKFold(n_splits=3, shuffle=True).split(X_train_enc, y_train)
+    #cv_scores = []
+    #test_scores = []
+    #for k, (train, test) in enumerate(kfold):
+    #    pipe.fit(X_train_enc[train], y_train[train])
+    #    cv_score = pipe.score(X_train_enc[test], y_train[test])
+    #    cv_scores.append(cv_score)
+    #    test_score = pipe.score(X_test_enc, y_test)
+    #    test_scores.append(test_score)
+    #    print('Fold: %s, Class dist.: %s, CV acc: %.3f, Tets acc: %.3f' % (k+1, np.bincount(y_train[train]), cv_score, test_score))
+        
+    print('done')
+    
 def main():
     
     train = pd.read_csv("train.csv")
---
+++ digits/gridsearch.py
@@ -0,0 +1,59 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.svm import SVC
+
+train = pd.read_csv("train.csv")
+y = train.values[:,0]
+X = train.values[:,1:]
+
+# split the training data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.333)
+
+# there are 784 dimensions (1 for each pixel)
+# each d has labels 0 to 255
+# one hot encode for each label
+labels = np.arange(0,256,1)
+enc_temp = labels.repeat(X_train.shape[1]).reshape((labels.shape[0], X_train.shape[1]))
+enc = OneHotEncoder(dtype=np.int8)
+enc.fit(enc_temp)
+X_train = enc.transform(X_train).toarray()
+X_test = enc.transform(X_test).toarray()
+
+# Set the parameters by cross-validation
+tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
+                     'C': [1, 10, 100, 1000]},
+                    {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
+
+scores = ['precision', 'recall']
+
+for score in scores:
+    print("# Tuning hyper-parameters for %s" % score)
+    print()
+
+    clf = GridSearchCV(SVC(C=1), tuned_parameters, cv=5,
+                       scoring='%s_macro' % score, n_jobs=-1)
+    clf.fit(X_train, y_train)
+
+    print("Best parameters set found on development set:")
+    print()
+    print(clf.best_params_)
+    print()
+    print("Grid scores on development set:")
+    print()
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean, std * 2, params))
+    print()
+
+    print("Detailed classification report:")
+    print()
+    print("The model is trained on the full development set.")
+    print("The scores are computed on the full evaluation set.")
+    print()
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print(classification_report(y_true, y_pred))
+    print()
