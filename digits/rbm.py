f334ef25556812b2f0c8550c5f4e5a7b6d6c1141
Rob Baxter <rbaxter8@gatech.edu>
2017-04-07 16:01:44 -0400

    grid

--- digits/rbm.py
+++ digits/rbm.py
@@ -111,7 +111,7 @@ def testRBM2():
     y = train.values[:,0]
     X = train.values[:,1:]
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.90)
     
     # there are 784 dimensions (1 for each pixel)
     # each d has labels 0 to 255
